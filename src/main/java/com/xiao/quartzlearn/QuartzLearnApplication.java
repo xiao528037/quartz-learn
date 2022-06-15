0b51719bfac875c6d8180ad18bd6765e5822e0f6
lucky <xiao528037@foxmail.com>
2022-06-15 10:53:15 +0800

    初始化项目

---
+++ src/main/java/com/xiao/quartzlearn/QuartzLearnApplication.java
@@ -0,0 +1,13 @@
+package com.xiao.quartzlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuartzLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xiao/quartzlearn/QuartzLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiao.quartzlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuartzLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
